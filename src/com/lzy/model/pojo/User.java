3cd8519bc71b178dba8c3abde1b8989d21e81fa5
longzy-hub <2587234011@qq.com>
2020-11-10 14:50:48 +0800

    add user

---
+++ src/com/lzy/model/pojo/User.java
@@ -0,0 +1,10 @@
+package com.lzy.model.pojo;
+
+public class User {
+	private Integer id;
+	private String openid;	// 用户的标识，对当前公众号唯一
+	private String nickname; // 用户的昵称
+	private String sex; // 性别
+	private String headimgurl; // 用户头像
+
+}
